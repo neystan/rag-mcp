f5de154c81d6bb097e941e799fe7286f377e90f5
neystan <2918360904@qq.com>
2026-08-10 20:38:56 +0800

    chore: keep tests and docs local

--- tests/test_runtime_dependencies.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import importlib.util
-
-
-def test_jieba_is_available_for_sparse_and_bm25_processing() -> None:
-    assert importlib.util.find_spec("jieba") is not None
